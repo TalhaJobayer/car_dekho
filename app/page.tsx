f997258a2a8a733d4a165cc29b7f73819f14234e
talha jobayer <talhajobayer8@gmail.com>
2024-02-17 22:50:01 +0600

    navbar and 'hero section applied

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Hero } from "@/components";
+import { Navbar } from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-       <Hero></Hero>
-          </main>
+    <main>
+      <Navbar></Navbar>
+      <Hero></Hero>
+
+    </main>
+         
   );
 }
